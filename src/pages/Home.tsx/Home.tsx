50beb6638a70f7dd843ac5dc83203a6a0805d709
Alex <alexperezdavies@gmail.com>
2024-06-11 15:46:50 +0100

    use context with products when adding and removing from cart

--- src/pages/Home.tsx/Home.tsx
+++ src/pages/Home.tsx/Home.tsx
@@ -1,4 +1,37 @@
+// We will need to replace with our actualy products
+import { products } from "../../data";
+import { useCart } from "../../ShoppingCartContext";
+
 function Home() {
-  return <h1>Home</h1>;
+  const { addToCart, removeFromCart } = useCart();
+
+  function handleAddToCart(productId: number) {
+    const product = products.find((p) => p.id === productId);
+    if (product) {
+      addToCart({ ...product, quantity: 1 });
+    }
+  }
+
+  function handleRemove(productId: number) {
+    const product = products.find((p) => p.id === productId);
+    if (product) {
+      removeFromCart(productId);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - £{product.price}
+            <button onClick={() => handleRemove(product.id)}>-</button>
+            <button onClick={() => handleAddToCart(product.id)}>+</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 export default Home;
