0b53c699f0139665fd746e80b0f5f6407caec2e3
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-17 17:44:02 +0100

    created product grid

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,5 +1,25 @@
+import './Product.css';
+// We will need to replace with our actual products
+import { products } from '../../data';
+import ProductCard from '../../components/ProductCard/ProductCard';
+
 function Product() {
-  return <h1>Product</h1>;
+	return (
+		<>
+			<h1>Products</h1>
+			<div className='products-grid'>
+				{products.map((product) => (
+					<ProductCard
+						key={product.id}
+						productName={product.name}
+						productCategory={product.category}
+						productPrice={product.price}
+						productImg={product.image}
+					/>
+				))}
+			</div>
+		</>
+	);
 }
 
 export default Product;
