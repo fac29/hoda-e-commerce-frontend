8d6de07341058dd48c94b1b079f136d180a9028b
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-17 17:44:20 +0100

    added Product component to Homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,10 @@
 // We will need to replace with our actualy products
-import { products } from '../../data';
-import ProductCard from '../../components/ProductCard/ProductCard';
+import Product from '../Product/Product';
 
 function Home() {
 	return (
 		<div>
-			<h1>Products</h1>
-			{products.map((product) => (
-				<ProductCard
-					key={product.id}
-					productName={product.name}
-					productCategory={product.category ?? 'Category'}
-					productPrice={product.price}
-					productImg={product.image ?? 'https://placehold.co/404x244'}
-				/>
-			))}
+			<Product />
 		</div>
 	);
 }
