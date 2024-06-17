059732b4cea6e0b0162b33c42c016aa6eb9e1511
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-17 16:48:55 +0100

    added variables to ProductCard

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,18 +1,45 @@
 import './ProductCard.css';
 import PlusMinusButton from '../PlusMinusButton/PlusMinusButton';
+import { products } from '../../data';
 import { useCart } from '../../ShoppingCartContext';
 // import { useNavigate } from 'react-router-dom';
 // import { products } from '../../data';
 
-function ProductCard() {
+type ProductProps = {
+	productId: number;
+	productName: string;
+	productCategory: string;
+	productPrice: number;
+	productImg: string;
+};
+
+function ProductCard({
+	productId,
+	productName,
+	productCategory,
+	productPrice,
+	productImg,
+}: ProductProps) {
+	const { addToCart } = useCart();
+
+	function handleAddToCart(productId: number) {
+		const product = products.find((p) => p.id === productId);
+		if (product) {
+			addToCart({ ...product, quantity: 1 });
+		}
+	}
+
 	return (
-		<div>
-			<img />
-			<h2 className='product-name'>Product Name</h2>
-			<h3 className='product-category'>Category</h3>
+		<div className='product-card'>
+			<img src={productImg} />
+			<h2 className='product-name'>{productName}</h2>
+			<h3 className='product-category'>{productCategory}</h3>
 			<div>
-				<span className='product-price'>Price</span>
-				<PlusMinusButton buttonText='+' />
+				<span className='product-price'>£{productPrice}</span>
+				<PlusMinusButton
+					buttonText='+'
+					buttonClick={() => handleAddToCart(productId)}
+				/>
 			</div>
 		</div>
 	);
