92f245607d91be9c347faa9ce5f72ede7a84458c
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-06-19 12:03:20 +0100

    format form

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -9,44 +9,54 @@ function SignUpForm() {
 
 	return (
 		<form action='submit' className='form'>
-			<label className='formLabel' htmlFor='username'>
-				Username
-			</label>
-			<input
-				className='formInput'
-				type='text'
-				name='username'
-				placeholder='username'
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
-			<label className='formLabel' htmlFor='email'>
-				Email Address
-			</label>
-			<input
-				className='formInput'
-				type='email'
-				name='email'
-				placeholder='Email Address'
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
-			/>
-			<label className='formLabel' htmlFor='password'>
-				Password
-			</label>
-			<input
-				className='formInput'
-				type='password'
-				name='password'
-				placeholder='Password'
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
+			<div className='formField'>
+				<label className='formLabel' htmlFor='username'>
+					Username
+				</label>
+				<input
+					className='formInput'
+					type='text'
+					name='username'
+					placeholder='username'
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
+			</div>
+			<div className='formField'>
+				{' '}
+				<label className='formLabel' htmlFor='email'>
+					Email Address
+				</label>
+				<input
+					className='formInput'
+					type='email'
+					name='email'
+					placeholder='Email Address'
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+			</div>
+			<div className='formField'>
+				<label className='formLabel' htmlFor='password'>
+					Password
+				</label>
+				<input
+					className='formInput'
+					type='password'
+					name='password'
+					placeholder='Password'
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+			</div>
 			<Button
 				buttonText='Sign Up'
 				buttonClick={() => handleSignUp()}
 				size='medium'
 			></Button>
+			<a className='formLink' href='/login'>
+				I have an account already. Take me to login.
+			</a>
 		</form>
 	);
 }
