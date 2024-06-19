b526954fd1efed4cd3e3dd6943e50e630b9125ab
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-19 13:03:13 +0100

    revised type Review to ReviewProps to avoid confusion

--- src/components/ReviewCard/ReviewCard.tsx
+++ src/components/ReviewCard/ReviewCard.tsx
@@ -1,6 +1,6 @@
 import './ReviewCard.css';
 
-export type Review = {
+export type ReviewProps = {
 	productId?: number;
 	reviewId: number;
 	reviewAuthor: string;
@@ -14,7 +14,7 @@ export function ReviewCard({
 	reviewAuthor,
 	reviewRating,
 	reviewComment,
-}: Review) {
+}: ReviewProps) {
 	return (
 		<div key={reviewId}>
 			<div className='flex'>
