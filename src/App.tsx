f4ea599d031f7f1b1f9eca9dc459a6fad20cc61e
diddydeeable <diddydeeable@gmail.com>
2024-06-19 19:38:44 +0100

    created template tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './routes/AppRoutes';
 
-import { ShoppingCartProvider } from "./ShoppingCartContext";
+import { ShoppingCartProvider } from './ShoppingCartContext';
+import NavBar from './components/NavBar/NavBar';
 
-import NavBar from "./components/NavBar/NavBar";
-
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <ShoppingCartProvider>
-        <Router>
-          <div>
-            <NavBar />
-            <AppRoutes />
-          </div>
-        </Router>
-      </ShoppingCartProvider>
-    </>
-  );
+	return (
+		<>
+			<ShoppingCartProvider>
+				<Router>
+					<div>
+						<NavBar />
+						<AppRoutes />
+					</div>
+				</Router>
+			</ShoppingCartProvider>
+		</>
+	);
 }
 
 export default App;
---
+++ src/pages/Product/Product.tsx
@@ -0,0 +1,18 @@
+import ReviewsList from '../../components/ReviewsList/ReviewsList';
+import 
+export function ProductPage() {
+	// I need to get the id (from image clicked)
+
+
+
+
+	return (
+		<>
+        <h1> product title</h1>
+        <img></img>
+        <span>product description</span>
+			<ReviewsList productId={3}></ReviewsList>
+           <div> add to cart</div> 
+		</>
+	);
+}
