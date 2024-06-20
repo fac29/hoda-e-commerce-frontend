6b59d932fc061fd23e26b4b657740d6e1fba5fe2
diddydeeable <diddydeeable@gmail.com>
2024-06-20 12:28:42 +0100

    remove unused code, fixed cart mapping

--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -1,61 +1,46 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
-// import { products } from "../../data";
-
-import { useCart } from "../../ShoppingCartContext";
-import { handleAddToCart, handleRemoveFromCart } from "../../utils/handleCart/handleCart";
+import { useCart } from '../../ShoppingCartContext';
 
 function ShoppingCart() {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  const { cart, total } = useCart();
-	const { addToCart, removeFromCart } = useCart();
-  function handleBack() {
-    navigate("/");
-  }
+	const { cart, total, handleRemoveFromCart } = useCart();
 
-  // const { addToCart, removeFromCart } = useCart();
+	function handleBack() {
+		navigate('/');
+	}
 
-  // function handleAddToCart(productId: number) {
-  //   const product = products.find((p) => p.id === productId);
-  //   if (product) {
-  //     addToCart({ ...product, quantity: 1 });
-  //   }
-  // }
+	// const { addToCart, removeFromCart } = useCart();
 
-  // function handleRemoveFromCart(productId: number) {
-  //   const product = products.find((p) => p.id === productId);
-  //   if (product) {
-  //     removeFromCart(productId);
-  //   }
-  // }
+	function goToCheckout() {
+		navigate('/checkout');
+	}
 
-  function goToCheckout() {
-    navigate("/checkout");
-  }
-
-  return (
-    <div>
-      <h2>Cart Items</h2>
-      <button onClick={handleBack}>Back to Products</button>
-      <ul>
-        {cart.length ? (
-          cart.map((item) => (
-            <li key={item.id}>
-              <p>{item.name}</p>
-              <button onClick={() => handleRemoveFromCart(item.id, removeFromCart)}>-</button>
-              <p>{item.quantity}</p>
-              <p>£{item.price * item.quantity}</p>
-            </li>
-          ))
-        ) : (
-          <h2>You don't have any items in your cart!</h2>
-        )}
-      </ul>
-      {cart.length ? <p>Total:£{total}</p> : null}
-      {cart.length ? <button onClick={goToCheckout}>Checkout</button> : null}
-    </div>
-  );
+	return (
+		<div>
+			<h2>Cart Items</h2>
+			<button onClick={handleBack}>Back to Products</button>
+			<ul>
+				{cart.length ? (
+					cart.map((item) => (
+						<li key={item.product_id}>
+							<p>{item.product_name}</p>
+							<button onClick={() => handleRemoveFromCart(item.product_id)}>
+								-
+							</button>
+							<p>{item.quantity}</p>
+							<p>£{item.price * item.quantity}</p>
+						</li>
+					))
+				) : (
+					<h2>You don't have any items in your cart!</h2>
+				)}
+			</ul>
+			{cart.length ? <p>Total:£{total}</p> : null}
+			{cart.length ? <button onClick={goToCheckout}>Checkout</button> : null}
+		</div>
+	);
 }
 
 export default ShoppingCart;
