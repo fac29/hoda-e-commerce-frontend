5d9edb8f8da99bce6fa36b4abc5750ee016272a0
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-24 16:50:27 +0100

    refactoring to add LogoutButton component

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,12 +2,13 @@ import { useNavigate, Link } from 'react-router-dom';
 import Button from '../Button/Button';
 import { IoCartOutline } from 'react-icons/io5';
 import { useCart } from '../../ShoppingCartContext';
+import LogoutButton from '../LogoutButton/LogoutButton';
 
 import './NavBar.css';
 
 function NavBar() {
 	const navigate = useNavigate();
-	const { loggedIn } = useCart();
+	const { loggedIn, username } = useCart();
 
 	function goToShoppingCart() {
 		navigate('/shopping-cart');
@@ -21,9 +22,10 @@ function NavBar() {
 		navigate('/signup');
 	}
 
-	function goToLogOut() {
-		navigate('/logout');
-	}
+	// A stretch goal to add list of user's orders
+	// function goToOrders() {
+	// 	navigate('/orders');
+	// }
 
 	return (
 		<nav className='nav-container'>
@@ -33,16 +35,30 @@ function NavBar() {
 				</Link>
 			</div>
 			<div className='nav-right'>
-				<Button
-					buttonText='Log In'
-					buttonClick={() => goToLogIn()}
-					size='small'
-				/>
-				<Button
-					buttonText='Sign Up'
-					buttonClick={() => goToSignUp()}
-					size='small'
-				/>
+				{loggedIn ? (
+					<>
+						<Button
+							buttonText={username + "'s Account"}
+							buttonClick={() => navigate('/')}
+							size='small'
+						/>
+
+						<LogoutButton />
+					</>
+				) : (
+					<>
+						<Button
+							buttonText='Log In'
+							buttonClick={() => goToLogIn()}
+							size='small'
+						/>
+						<Button
+							buttonText='Sign Up'
+							buttonClick={() => goToSignUp()}
+							size='small'
+						/>
+					</>
+				)}
 				<Button
 					buttonText='Cart'
 					buttonClick={() => goToShoppingCart()}
