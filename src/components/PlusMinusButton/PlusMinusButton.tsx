2dd5109ced634b4b516264f6058b1e1d3167b10d
diddydeeable <diddydeeable@gmail.com>
2024-06-24 15:12:54 +0100

    did minimal styling

--- src/components/PlusMinusButton/PlusMinusButton.tsx
+++ src/components/PlusMinusButton/PlusMinusButton.tsx
@@ -1,18 +1,19 @@
-import "./PlusMinusButton.css";
+import './PlusMinusButton.css';
 
 type plusMinusButtonProps = {
-  buttonText: string;
-  buttonClick?: () => void;
+	className?: string;
+	buttonText: string;
+	buttonClick?: () => void;
 };
 
 function PlusMinusButton({ buttonText, buttonClick }: plusMinusButtonProps) {
-  return (
-    <>
-      <button className="plus-minus-button" onClick={buttonClick}>
-        {buttonText}
-      </button>
-    </>
-  );
+	return (
+		<>
+			<button className={'plus-minus-button'} onClick={buttonClick}>
+				{buttonText}
+			</button>
+		</>
+	);
 }
 
 export default PlusMinusButton;
--- src/components/ProductDetailCard/ProductDetails.tsx
+++ src/components/ProductDetailCard/ProductDetails.tsx
@@ -1,6 +1,8 @@
 import { Product } from '../../utils/dataTypes/product';
 import ReviewsList from '../ReviewsList/ReviewsList';
-
+import PlusMinusButton from '../PlusMinusButton/PlusMinusButton';
+import { useCart } from '../../ShoppingCartContext';
+import './ProductDetailCard.css';
 
 type ProductDetailCardProps = {
 	product_id: Product['product_id'];
@@ -19,30 +21,49 @@ export function ProductDetailCard({
 	category,
 	product_description,
 	price,
-    product_image
+	product_image,
 }: ProductDetailCardProps) {
+	const { handleRemoveFromCart, handleAddToCart } = useCart();
 
 	return (
 		<>
 			<h2> See the book in more detail!</h2>
 			{product_name ? (
 				<>
+					<>
+						<div className='buttons'>
+							<div className='alignBtnWText'>
+								<span>Add 1 to cart</span>
+								<PlusMinusButton
+									buttonText='+'
+									buttonClick={() => handleAddToCart(product_id)}
+								/>
+							</div>
+							<div className='product-image'>
+								<img style={{ maxHeight: 200 }} src={'/' + product_image} />
+							</div>
+							<div className='alignBtnWText'>
+								<span>remove 1 from cart</span>
+								<PlusMinusButton
+									className='buttons'
+									buttonText='-'
+									buttonClick={() => handleRemoveFromCart(product_id)}
+								/>
+							</div>
+						</div>
+					</>
 					<ul>
 						<li className='product-name'>{product_name}</li>
 						<li className='product- author'>{product_author}</li>
 						<li className='product-category'>{category}</li>
 						<li className='product-description'>{product_description}</li>
 						<li className='product-price'>£{price / 100}</li>
-						<li className='product-image'>
-							<img style={{ maxHeight: 200 }} src={'/' + product_image} />
-						</li>
 					</ul>
 				</>
 			) : (
 				<p>Loading...</p>
 			)}
 
-			<div> add to cart</div>
 			<div className='reviews-container'>
 				<ReviewsList productId={product_id}></ReviewsList>
 				<div />
