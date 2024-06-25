94f6707326c14d1991a55905d64ccc6614794fbf
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-25 12:34:30 +0100

    removed console logs

--- src/utils/fetchSession/fetchSession.ts
+++ src/utils/fetchSession/fetchSession.ts
@@ -2,15 +2,11 @@ export async function fetchSessionById() {
 	const response = await fetch(`http://localhost:3000/session`, {
 		credentials: 'include',
 	});
-	console.log('Response Status:', response.status);
-	console.log('Response Headers:', response.headers);
 
 	if (!response.ok) {
 		throw new Error(`HTTP error! status: ${response.status}`);
 	} else {
-		console.log(response);
 		const data = await response.json();
-		console.log(data);
 		return data;
 	}
 }
