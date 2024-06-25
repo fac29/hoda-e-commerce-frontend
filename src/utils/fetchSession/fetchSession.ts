3fbad32bf1dcdc5e5d8b6ba7debc890481e6a94e
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-25 12:34:11 +0100

    fixed function so it works with cookies

--- src/utils/fetchSession/fetchSession.ts
+++ src/utils/fetchSession/fetchSession.ts
@@ -1,11 +1,16 @@
-export async function fetchSessionById(id: number) {
-	const response = await fetch(`http://localhost:3000/session/${id}`);
+export async function fetchSessionById() {
+	const response = await fetch(`http://localhost:3000/session`, {
+		credentials: 'include',
+	});
+	console.log('Response Status:', response.status);
+	console.log('Response Headers:', response.headers);
 
 	if (!response.ok) {
 		throw new Error(`HTTP error! status: ${response.status}`);
+	} else {
+		console.log(response);
+		const data = await response.json();
+		console.log(data);
+		return data;
 	}
-
-	console.log(response);
-	const data = await response.json();
-	return data;
 }
