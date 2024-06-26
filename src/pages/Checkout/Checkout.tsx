81373bc17d5e38f0902609ae8c0f4361f3387631
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-26 21:07:31 +0100

    added OrderTable component

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -1,4 +1,31 @@
+import OrderTable from '../../components/OrderTable/OrderTable';
+import { useCart } from '../../ShoppingCartContext';
+
+// const fetchOrderDetails = () => {};
+
+// const order: OrderDetails = {
+// 	orderID: 20,
+// 	products: [
+// 		{
+// 			productName: 'The Great Gatsby',
+// 			price: 1099,
+// 			quantity: 2,
+// 		},
+// 	],
+// };
+
 function Checkout() {
-  return <h1>Checkout</h1>;
+	const { order } = useCart();
+
+	return (
+		<>
+			<h1>Checkout</h1>
+			{order ? (
+				<OrderTable order_id={order.order_id} products={order.products} />
+			) : (
+				''
+			)}
+		</>
+	);
 }
 export default Checkout;
