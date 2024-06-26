2d4cb1cfd8b91d11ca3c0a201cf82c8cabd0fea9
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-26 19:51:48 +0100

    refactored to include loggedIn check in handleCheckout to ensure orders are not submitted without log in

--- src/pages/ShoppingCart/ShoppingCart.tsx
+++ src/pages/ShoppingCart/ShoppingCart.tsx
@@ -4,80 +4,103 @@ import { useCart } from '../../ShoppingCartContext';
 import PlusMinusButton from '../../components/PlusMinusButton/PlusMinusButton';
 import { Modal } from '../../components/Modal/Modal';
 import { useState } from 'react';
-// const requestUrl = import.meta.env.VITE_REQUEST_URL;
+const requestUrl = import.meta.env.VITE_REQUEST_URL;
 
 function ShoppingCart() {
-    const navigate = useNavigate();
-    const [isOpen, setIsOpen] = useState(false);
-    const {
-        cart,
-        total,
-        handleRemoveFromCart,
-        handleAddToCart,
-        loggedIn,
-        // userID,
-    } = useCart();
+	const navigate = useNavigate();
+	const [isOpen, setIsOpen] = useState(false);
+	const {
+		cart,
+		resetCart,
+		total,
+		handleRemoveFromCart,
+		handleAddToCart,
+		loggedIn,
+		userID,
+	} = useCart();
 
-    function handleBack() {
-        navigate('/');
-    }
+	function handleBack() {
+		navigate('/');
+	}
 
-    function goToCheckout() {
-        if (loggedIn) {
-            navigate('/checkout');
-        } else {
-            setIsOpen(true);
-        }
-    }
+	// function goToCheckout() {
+	// 	if (loggedIn) {
+	// 		navigate('/checkout');
+	// 		resetCart();
+	// 	} else {
+	// 		setIsOpen(true);
+	// 	}
+	// }
 
-    //   async function handleCheckout() {
-    //     const response = await fetch(`${requestUrl}/checkout`, {
-    //       method: "POST",
-    //     });
-    //   }
+	async function handleCheckout() {
+		if (loggedIn) {
+			const products = [];
+			for (const item of cart) {
+				const product = {
+					product_id: item.product_id,
+					product_name: item.product_name,
+					price: item.price,
+					quantity: item.quantity,
+				};
+				products.push(product);
+			}
+			console.log('UserID:', userID);
+			const response = await fetch(`${requestUrl}/checkout`, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ userID, products }),
+				credentials: 'include',
+			});
 
-    return (
-        <div>
-            <h2>Cart Items</h2>
-            <button onClick={handleBack}>Back to Products</button>
-            <ul>
-                {cart.length ? (
-                    cart.map((item) => (
-                        <li key={item.product_id}>
-                            <p>{item.product_name}</p>
-                            <PlusMinusButton
-                                buttonText='-'
-                                buttonClick={() =>
-                                    handleRemoveFromCart(item.product_id)
-                                }
-                            />
-                            <PlusMinusButton
-                                buttonText='+'
-                                buttonClick={() =>
-                                    handleAddToCart(item.product_id)
-                                }
-                            />
+			if (response.ok) {
+				const data = await response.json();
+				console.log('Order successful:', data);
+				navigate('/checkout');
+				resetCart();
+			} else {
+				const errorData = await response.json();
+				console.error('Checkout failed:', errorData.response);
+			}
+		} else {
+			setIsOpen(true);
+		}
+	}
 
-                            <p>{item.quantity}</p>
-                            <p>£{(item.price * item.quantity) / 100}</p>
-                        </li>
-                    ))
-                ) : (
-                    <h2>You don't have any items in your cart!</h2>
-                )}
-            </ul>
-            {cart.length ? <p>Total: £{total ? total / 100 : null}</p> : null}
-            {isOpen && (
-                <Modal
-                    textModal='Please Log In to Checkout'
-                    setIsOpen={setIsOpen}
-                />
-            )}
-            {cart.length ? (
-                <button onClick={goToCheckout}>Checkout</button>
-            ) : null}
-        </div>
-    );
+	return (
+		<div>
+			<h2>Cart Items</h2>
+			<button onClick={handleBack}>Back to Products</button>
+			<ul>
+				{cart.length ? (
+					cart.map((item) => (
+						<li key={item.product_id}>
+							<p>{item.product_name}</p>
+							<PlusMinusButton
+								buttonText='-'
+								buttonClick={() => handleRemoveFromCart(item.product_id)}
+							/>
+							<PlusMinusButton
+								buttonText='+'
+								buttonClick={() => handleAddToCart(item.product_id)}
+							/>
+
+							<p>{item.quantity}</p>
+							<p>£{(item.price * item.quantity) / 100}</p>
+						</li>
+					))
+				) : (
+					<h2>You don't have any items in your cart!</h2>
+				)}
+			</ul>
+			{cart.length ? <p>Total: £{total ? total / 100 : null}</p> : null}
+			{isOpen && (
+				<Modal textModal='Please Log In to Checkout' setIsOpen={setIsOpen} />
+			)}
+			{cart.length ? <button onClick={handleCheckout}>Checkout</button> : null}
+		</div>
+	);
 }
 
 export default ShoppingCart;
