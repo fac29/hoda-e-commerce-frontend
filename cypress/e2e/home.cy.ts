3a19790a102ad2728472f0fcb0e0d127788e50a4
Oskar Przybylski <oskar.przybylski23@gmail.com>
2024-06-26 13:32:58 +0100

    add tests for homepage and sign up form

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,44 @@
-describe('home page test', () => {
-	it('passes', () => {
+describe('home page', () => {
+	beforeEach(() => {
 		cy.visit('/');
+	});
+
+	it('should contain a correct header', () => {
+		cy.get('h1').contains('Products');
+	});
+
+	it('should display the navigation buttons', () => {
+		cy.get('.nav-right button').contains('Log In').should('be.visible');
+		cy.get('.nav-right button').contains('Sign Up').should('be.visible');
+		cy.get('.nav-right button').contains('Cart').should('be.visible');
+	});
+
+	it('should search for a product', () => {
+		cy.get('.search-bar input').type('Gatsby');
+		cy.get('.search-bar button').contains('Search').click();
+		cy.get('.product-card .product-name').should('contain', 'The Great Gatsby');
+	});
+
+	it('should display a list of products', () => {
+		cy.get('.products-grid .product-card').should('have.length.at.least', 1);
+		cy.get('.product-card').each((product) => {
+			cy.wrap(product).find('.product-name').should('be.visible');
+			cy.wrap(product).find('.product-category').should('be.visible');
+			cy.wrap(product).find('.product-price').should('be.visible');
+		});
+	});
+
+	it('should navigate to product detail page', () => {
+		cy.get('.product-card').first().find('.product-button').click();
+		// cy.url().should('include', '/products/1');
+		// cy.get('.product-name').should('be.visible');
+	});
+
+	it('should add a product to the cart', () => {
+		cy.get('.product-card')
+			.first()
+			.within(() => {
+				cy.get('.plus-minus-button').click();
+			});
 	});
 });
---
+++ cypress/e2e/user.cy.ts
@@ -0,0 +1,70 @@
+describe('user actions', () => {
+	beforeEach(() => {
+		cy.visit('/signup');
+	});
+
+	it('should display the Sign Up form', () => {
+		cy.contains('h1', 'SignUp').should('be.visible');
+		cy.get('form.form').should('exist');
+	});
+
+	it('should fill out and submit the Sign Up form', () => {
+		// Generate unique username and email using timestamps
+		const username = `testuser_${Date.now()}`;
+		const email = `test_${Date.now()}@example.com`;
+
+		// Fill out the form with unique username, email, and valid password
+		cy.get('input[name="username"]').type(username);
+		cy.get('input[name="email"]').type(email);
+		cy.get('input[name="password"]').type('validpassword123');
+
+		// Click Sign Up button
+		cy.get('button.medium').contains('Sign Up').click();
+
+		// Assert successful navigation after submission
+		cy.url().should('include', '/');
+	});
+
+	it('should navigate to login page from Sign Up form', () => {
+		cy.contains('I have an account already. Take me to login.')
+			.should('have.attr', 'href', '/login')
+			.click();
+
+		cy.url().should('include', '/login');
+	});
+
+	it('should display validation errors for empty username, invalid email, and short password', () => {
+		// Click Sign Up button without filling out the form
+		cy.get('button.medium').contains('Sign Up').click();
+
+		// Assert validation errors are displayed
+		cy.contains('Please enter a valid email address.').should('be.visible');
+		cy.contains('Password must be at least 8 characters long.').should(
+			'be.visible'
+		);
+
+		// Fill out the form with invalid data
+
+		cy.get('input[name="email"]').type('invalidemail'); // Invalid email format
+		cy.get('input[name="password"]').type('123'); // Short password
+
+		// Click Sign Up button again
+		cy.get('button.medium').contains('Sign Up').click();
+
+		// Assert validation errors are displayed
+		cy.contains('Please enter a valid email address.').should('be.visible');
+		cy.contains('Password must be at least 8 characters long.').should(
+			'be.visible'
+		);
+
+		// Correct the email and password
+		cy.get('input[name="email"]').clear().type('validemail@example.com');
+		cy.get('input[name="password"]').clear().type('validpassword123');
+
+		// Click Sign Up button with valid data
+		cy.get('button.medium').contains('Sign Up').click();
+
+		// Assert the form should navigate to login page after successful submission
+		cy.url().should('include', '/');
+	});
+});
